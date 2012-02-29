5150c2ce10247e5cb91cc62e41dd840f121937aa
Russell Spitzer <russell.spitzer@gmail.com>
2012-02-28 18:47:27 -0800

    Poop

---
+++ BrothersOfMagic/src/brothersofmagic/BrothersOfMagic.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brothersofmagic;
+
+/**
+ *
+ * @author MagRus
+ */
+public class BrothersOfMagic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("POOP");
+        // TODO code application logic here
+    }
+}
